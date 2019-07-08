4419b111661a440b9ddaa9cbeb5bb58981b97667
nagasaka2410 <warkun20@gmail.com>
2019-07-08 20:31:52 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,11 @@ Rails.application.routes.draw do
   }
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
+  resources :users, only: [:index, :show] do
     collection do
       get :identification
     end
   end
-
-
-
 
   resources :products do
     collection do
