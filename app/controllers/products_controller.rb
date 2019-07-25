ca324ee4c5148b18d820afc59b8ce68d7902dab8
nagasaka2410 <warkun20@gmail.com>
2019-07-25 21:31:17 +0900

    トップページにDBから引っ張って表示

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,14 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
-    @parents = Category.order("id ASC").limit(13)
+    @lady_items = Product.includes(:images).where(category_id: Category.find(1).subtree_ids).where(status: 1).order(created_at: "DESC").limit(4)
+    @man_items = Product.includes(:images).where(category_id: Category.find(2).subtree_ids).where(status: 1).order(created_at: "DESC").limit(4)
+    @kids_items = Product.includes(:images).where(category_id: Category.find(3).subtree_ids).where(status: 1).order(created_at: "DESC").limit(4)
+    @beauty_items = Product.includes(:images).where(category_id: Category.find(7).subtree_ids).where(status: 1).order(created_at: "DESC").limit(4)
+    
+    @chanel = Product.includes(:images).where(brand_id: 1).where(status: 1).order(created_at: "DESC").limit(4)
+    @vuitton = Product.includes(:images).where(brand_id: 2).where(status: 1).order(created_at: "DESC").limit(4)
+    @supreme = Product.includes(:images).where(brand_id: 3).where(status: 1).order(created_at: "DESC").limit(4)
+    @nike = Product.includes(:images).where(brand_id: 4).where(status: 1).order(created_at: "DESC").limit(4)
   end
   
   def new
