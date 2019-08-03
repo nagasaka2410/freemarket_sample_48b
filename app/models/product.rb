92c96d875620c2fe4e68d540bed19c4dd5def41d
nagasaka2410 <warkun20@gmail.com>
2019-08-03 13:39:19 +0900

    コンフリクト後の編集

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 has_many :product_commets,dependent: :destroy
-has_many :images
+has_many :images, dependent: :destroy
 accepts_nested_attributes_for :images
 belongs_to :user
 belongs_to :category
