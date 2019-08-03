dccbc95e470d6c9705252c30babad37ad63714bd
masakigithub <gtrist_gtdx@yahoo.co.jp>
2019-08-03 14:07:35 +0900

    リダイレクト修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -20,7 +20,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      # redirect_to new_user_registration_url
+      redirect_to new_user_registration_path
     end
   end
 end
