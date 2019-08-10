56078e6ead73440c6ec279b0f2789121bab6d0fd
y-masaya <my.927ac@gmail.com>
2019-08-10 20:08:38 +0900

    修正７

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   # has_many :product_commets,dependent: :destroy
+  # コメント機能の実装してないため
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images
   belongs_to :user
