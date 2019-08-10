92f34da99a79aa2da66f0037f841c163ba1ccf18
y-masaya <my.927ac@gmail.com>
2019-08-10 19:31:56 +0900

    修正５

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,22 +33,22 @@ class ProductsController < ApplicationController
     # 親カテゴリーが選択された後に動くアクション
   def get_category_children
     #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-    @category_children = Category.find_by(id: "#{params[:parent_id]}", ancestry: nil).children
+    @category_children = Category.find_by(params[:parent_id], ancestry: nil).children
   end
 
 
   def get_category_grandchildren
     #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
+    @category_grandchildren = Category.find(params[:child_id]).children
   end
 
 
   def get_size
-    selected_grandchild = Category.find("#{params[:grandchild_id]}") #孫カテゴリーを取得
+    selected_grandchild = Category.find(params[:grandchild_id]) #孫カテゴリーを取得
     if related_size_parent = selected_grandchild.products_sizes[0] #孫カテゴリーと紐付くサイズ（親）があれば取得
        @sizes = related_size_parent.children #紐づいたサイズ（親）の子供の配列を取得
     else
-       selected_child = Category.find("#{params[:grandchild_id]}").parent #孫カテゴリーの親を取得
+       selected_child = Category.find(params[:grandchild_id]).parent #孫カテゴリーの親を取得
        if related_size_parent = selected_child.products_sizes[0] #孫カテゴリーの親と紐付くサイズ（親）があれば取得
           @sizes = related_size_parent.children #紐づいたサイズ（親）の子供の配列を取得
        end
