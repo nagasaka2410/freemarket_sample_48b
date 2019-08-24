4415d2a3ed9a1c304e858f785ce5ad367ce44710
nagasaka2410 <warkun20@gmail.com>
2019-08-24 12:08:41 +0900

    ブランド修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,9 @@ class ProductsController < ApplicationController
     @beauty_items = Product.includes(:images).where(category_id: Category.find(7).subtree_ids, status: 0).order(created_at: "DESC").limit(4)
     
     @chanel = Product.includes(:images).where(brand_id: 1, status: 0).order(created_at: "DESC").limit(4)
-    @vuitton = Product.includes(:images).where(brand_id: 2, status: 0).order(created_at: "DESC").limit(4)
-    @supreme = Product.includes(:images).where(brand_id: 3, status: 0).order(created_at: "DESC").limit(4)
-    @nike = Product.includes(:images).where(brand_id: 4, status: 0).order(created_at: "DESC").limit(4)
+    @nike = Product.includes(:images).where(brand_id: 2, status: 0).order(created_at: "DESC").limit(4)
+    @vuitton = Product.includes(:images).where(brand_id: 3, status: 0).order(created_at: "DESC").limit(4)
+    @supreme = Product.includes(:images).where(brand_id: 4, status: 0).order(created_at: "DESC").limit(4)
   end
   
   def new
